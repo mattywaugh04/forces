c48a1f0f033879c79eef3a2ac628c5bc6e59ca71
Matthew Waugh <RC0042419@student.riversidecollege.ac.uk>
2021-05-10 15:43:07 +0100

    Add project files.

---
+++ forces/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.00));
+        }
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+        }
+        double sin(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.00));
+        }
+        double asin(double x)
+        {
+            return (Math.Acos(x) * 180 / Math.PI);
+        }
+
+
+
+            private void PictureBox1_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void Button1_Click(object sender, EventArgs e)
+            {
+                double force = double.Parse(TextBox1.Text);
+                Double Angle = double.Parse(TextBox2.Text);
+
+                double Fx = force * cos(Angle);
+                double Fy = Angle * sin(Angle);
+                //
+                label1.Text = "fx= " + Fx;
+                label2.Text = "Fy= " + Fx;
+            }
+        }
+    }
+}
